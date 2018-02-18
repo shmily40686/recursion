c6d4afe439c141ff3d34599fabada2ec19b3fdc8
Frog <shmily40686@gmail.com>
2018-02-17 22:19:06 -0800

    finished

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,5 +7,36 @@ var stringifyJSON = function(obj) {
   // your code goes here
   if (typeof obj === 'number') {
     return obj.toString();
+  } else if ( typeof obj === 'boolean' ){
+    return obj.toString();
+  } else if ( obj===null ){
+    return "null";
+  } else if ( typeof obj === 'string' ){
+    return '"'+obj+'"';
+  } else if ( Array.isArray(obj) ){
+    if ( obj.length===0 ){
+      return '[]';
+    } else {
+      var returnString='[';
+      for( var i =0 ; i <obj.length; i++){
+        returnString += stringifyJSON(obj[i]);
+        if (i < obj.length - 1) {
+          returnString += ',';
+        }
+      }
+      return returnString+']';
+    }
+  } else if (typeof obj ==='object'){
+    var returnObj='{';
+    for( var key in obj){
+      if ( typeof obj[key] === 'function' || obj[key] === undefined ){
+      } else {
+        returnObj += '"' + key + '":' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    if (returnObj.length ===1){
+      return '{}'; 
+    }
+    return returnObj.substr(0,returnObj.length-1) + '}';
   }
 };
